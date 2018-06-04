4eb8aebde6b2f79b360c63460152c1ca83c9d29c
David Tenenbaum <github email address>
2018-06-04 17:03:39 -0400

    Done

--- app.rb
+++ app.rb
@@ -1,8 +1,17 @@
 class App < Sinatra::Base
 
-	get '/' do
-		erb :index
+	get '/hello' do
+		erb :hello
 	end
 
+	get '/goodbye' do
+		@name = "Joe"
+		erb :goodbye
+	end
+
+	get '/date' do
+		@date = Date.today.strftime("%A, %B %d, %Y")
+		erb :date
+	end
 
 end
